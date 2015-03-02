8f0aa3baeb9ba8bec54cb93e791da9632f70aa5a
froatsnook <froatsnook@gmail.com>
2015-03-02 09:33:48 +0100

    Rename async function parameter.

--- server/lib/sleep.js
+++ server/lib/sleep.js
@@ -1,12 +1,12 @@
-var caller = function(ms, callback) {
+var asyncSleep = function(ms, callback) {
     Meteor.setTimeout(function() {
         callback(null);
     }, ms);
 };
 
 if (typeof Meteor.wrapAsync !== "undefined") {
-    Meteor.sleep = Meteor.wrapAsync(caller);
+    Meteor.sleep = Meteor.wrapAsync(asyncSleep);
 } else {
-    Meteor.sleep = Meteor._wrapAsync(caller);
+    Meteor.sleep = Meteor._wrapAsync(asyncSleep);
 }
 
